dd9f682a76ce254fba3bb3414fcb2199c1c54597
Adam Gordon <adamjgordon24@gmail.com>
2020-12-27 19:50:08 -0500

    created user model

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const getDb = require('./db.js').getDb
 const cors = require('cors')
 const express = require('express');
 
+const User = require('./models/user')
+
+
 const app = express();
+const bodyParser = require('body-parser');
 
 
 app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoConnect(()=>{
- app.listen(1000)
+mongoConnect(() => {
+    app.listen(1000)
 })
 
 
@@ -19,15 +25,38 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     const db = getDb()
-    console.log({db})
     const users = db.collection('users').find().toArray()
     return users
-    .then(result => {
-        console.log({result})
-        res.send(result)
-    })
-    .catch(err => {
-        res.send("IT didn't work")
-        console.log({err})
-    })
+        .then(result => {
+            // console.log({ result })
+            res.send(result)
+        })
+        .catch(err => {
+            res.send("IT didn't work")
+            // console.log({ err })
+        })
 });
+
+app.post('/users', (req, res) => {
+    const db = getDb()
+    const { username, gameWordArray, gameScore } = req.body;
+    const user = db.collection('users').findOne({ email: username })
+
+    return user
+        .then(result => {
+            console.log({ result })
+            const { abswordity: { scores, longestWords, highestScoringWords } } = result;
+
+            const submittedUser = new User(result)
+
+            submittedUser.updateTopFiveScores(gameScore)
+            submittedUser.save();
+        })
+        .catch(err => {
+            console.log({ err })
+        })
+
+    // need to figure out how to pass info back to client
+    res.write("HELLO")
+
+})
--- db.js
+++ db.js
@@ -4,22 +4,21 @@ const MongoClient = mongodb.MongoClient;
 
 let _db;
 const mongoConnect = callback => {
-	MongoClient.connect(
-        'mongodb+srv://adamjgordon30:H%40mbone11235@abswordity0-hthgk.mongodb.net/test?retryWrites=true&w=majority'
-
-  	)
-    .then(client => {
-        _db = client.db('Abswordity-V1');
-        callback();
-    })
-    .catch(err => {
-        console.log(err);
-        throw err;
-    });
+    MongoClient.connect(
+        'mongodb+srv://adamjgordon30:H%40mbone11235@abswordity0-hthgk.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true }
+    )
+        .then(client => {
+            _db = client.db('Abswordity-V1');
+            callback();
+        })
+        .catch(err => {
+            // console.log(err);
+            throw err;
+        });
 };
 
 const getDb = () => {
-    if(_db){
+    if (_db) {
         return _db;
     }
     throw 'No database found!'
