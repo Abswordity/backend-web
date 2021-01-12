2bf2911fdbf655be559b9c004729b4042933f02a
github username <44593008+tzip25@users.noreply.github.com>
2021-01-11 21:09:46 -0500

    adds functions to update top word scores and lengths

--- models/user.js
+++ models/user.js
@@ -19,12 +19,29 @@ class User {
     }
 
     updateTopFiveScores = (currentScore) => {
-        const db = getDb()
         const previousScores = this.abswordity.scores;
         previousScores.push(currentScore);
         previousScores.sort(function (a, b) { return b - a })
         const newScores = previousScores.slice(0, 5)
         this.abswordity.scores = newScores;
+        return;
+    }
+
+    updateTopFiveLongestWords = (longestWordsArray) => {
+        const previousLongestWords = this.abswordity.longestWords;
+        const combinedArray = previousLongestWords.concat(longestWordsArray);
+        combinedArray.sort(function (a, b) { return b.length - a.length })
+        const newLongestWords = combinedArray.slice(0,5)
+        this.abswordity.longestWords = newLongestWords;
+        return;
+    }
+
+    updateTopFiveHighestScoringWords = (highestScoringWordsArray) => {
+        const previousHighestScoringWords = this.abswordity.highestScoringWords;
+        const combinedArray = previousHighestScoringWords.concat(highestScoringWordsArray);
+        combinedArray.sort(function (a, b) { return b.score - a.score });
+        const newHighestScoringWords = combinedArray.slice(0,5)
+        this.abswordity.highestScoringWords = newHighestScoringWords;
         return;
     }
 };
